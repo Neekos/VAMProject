fe38da440d1d9dc2767111585d2afa6dbc8f9180
Андрей Спиридонов <and1997310366@mail.ru>
2019-10-07 23:01:50 +0700

    Стиль авторизации
    Косячно пока что ))

--- view/sign/signin.php
+++ view/sign/signin.php
@@ -5,18 +5,28 @@
 
 
 
-<div class="container-fluid">
+		<div class="mychange">
+			<form method="POST" class="form">
+				<h3 class="form_title">Авторизация</h3>
+		<div class="group">
+			<input class="form_input" type="text" name="username" placeholder="Логин">
+			<label class="form_label">Логин</label>	
+		</div>
+	    <div class="group">
+	    	<input class="form_input" type="password" name="password" placeholder="Пароль">
+	    	<label class="form_label">Пароль</label>	
+	    </div>
+	    <br>
 
-  <form method="POST">
-    <h3>Авторизация</h3>
-    <input type="text" name="username" placeholder="Логин"><br><br>
-    <input type="password" name="password" placeholder="Пароль"><br>
-    <a href="/signup/">Регистрация</a><br>
-    <input type="submit" name="signin">
+	    	<a href="/signup/">Регистрация</a>
+		    <input class="form_button signin" type="submit" name="signin">	
 
-  </form>
+	    
 
-</div>
+	  </form>
+	
+		</div>
+				
    
 
 
