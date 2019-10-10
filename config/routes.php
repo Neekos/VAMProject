c30a0ee130c7682b7a61b537c0abbab546896230
RodriguezLaCortellez <belskih15@gmail.com>
2019-10-10 20:48:00 +0800

    РОуТ почему он меняется при морже

--- config/routes.php
+++ config/routes.php
@@ -1,18 +1,16 @@
 <?php
 
 return array(
-<<<<<<< HEAD
 
-=======
->>>>>>> Андрей
+
+
 	'def'=>'def/index',
 	'about'=>'about/index',
 	'price'=>'price/index',
 	'galereya'=>'galereya/index',
-<<<<<<< HEAD
+
 	'cabinet/cms' => 'cabinet/cms',
-=======
->>>>>>> Андрей
+
 	'cabinet/diary' => 'cabinet/diary',
 	'cabinet' => 'cabinet/index',
 	'logout' => 'sign/logout',
