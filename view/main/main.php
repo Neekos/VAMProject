61c8b5ba2ca078e60e7708d1cd0b35b75d7712dd
Андрей Спиридонов <and1997310366@mail.ru>
2019-10-14 22:41:11 +0700

    css

--- view/main/main.php
+++ view/main/main.php
@@ -5,11 +5,13 @@
 
   <div class="intro">
       <div class="container">
-        <div class="intro__inner">
-          <h2 class="intro__suptitle">Creating website for you</h2>
-          <h1 class="intro__title">Welcome to VAMProject</h1>
+        <div class="col-md 12">
+          <div class="intro__inner">
+            <h2 class="intro__suptitle">Creating website for you</h2>
+            <h1 class="intro__title">Welcome to VAMProject</h1>
 
-          <a href="" class="btn__inner">Заказать</a>
+            <a href="" class="btn__inner">Заказать</a>
+          </div>
         </div>        
       </div>
   </div>
