2066b4f29e5a2372c256f9ec3aa5baf2afe8d531
Андрей Спиридонов <and1997310366@mail.ru>
2019-10-15 10:50:07 +0700

    Подготовка дизайна

--- view/main/main.php
+++ view/main/main.php
@@ -5,16 +5,30 @@
 
   <div class="intro">
       <div class="container">
-        <div class="col-md 12">
-          <div class="intro__inner">
-            <h2 class="intro__suptitle">Creating website for you</h2>
-            <h1 class="intro__title">Welcome to VAMProject</h1>
+        <div class="row">
+          <div class="col-md 12">
+            <div class="intro__inner">
+              <h2 class="intro__suptitle">Creating website for you</h2>
+              <h1 class="intro__title">Welcome to VAMProject</h1>
 
-            <a href="" class="btn__inner">Заказать</a>
+              <a href="" class="btn__inner">Заказать</a>
+            </div>
           </div>
         </div>        
       </div>
   </div>
+
+  <section class="section">
+    <div class="container">
+      <div class="section__header">
+        <h3 class="section__suptitle">Чем мы занимаемся</h3>
+        <h2 class="section__title" >Наши услуги</h2>
+        <div class="section__text">
+          <p class="section__text__pp">Мы молодая и активная команда разработчиков которая может предложить вам разработку сайтов по низкой цене, а так же сопровождение проектов, их модернизацию и перенос на другие движки.</p>
+        </div>
+      </div>
+    </div>
+  </section>
   
 
 <?php 
